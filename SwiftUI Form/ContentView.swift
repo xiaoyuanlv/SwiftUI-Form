dc2bdec31a16379ef2e4bfc5fb36662511dbacd8
Xiao Yuan Lv <xiaoyuanlv.31@gmail.com>
2021-12-17 19:29:48 +0630

    First Commit

--- SwiftUI Form/ContentView.swift
+++ SwiftUI Form/ContentView.swift
@@ -8,10 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var birthdate = Date()
+    @State private var shouldSendNewsletter = false
+    @State private var numberOfLikes = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Personal Information")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    DatePicker("Birthdate", selection: $birthdate, displayedComponents: .date)
+                }
+                
+                Section(header: Text("Actions")) {
+                    Toggle("Send Newsletter", isOn: $shouldSendNewsletter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of Likes", value: $numberOfLikes, in: 1...100)
+                    Text("This video has \(numberOfLikes) likes")
+                    Link("Terms of Service", destination: URL(string: "https://www.google.com")!)
+                }
+            }
+            .navigationTitle("Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        hideKeyword()
+                    } label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                    
+                    Button("Save", action: saveUser)
+                }
+            }
+            .accentColor(.red)
+        }
+        
     }
+    
+    func saveUser() {
+        print("User Saved")
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +60,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyword() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
