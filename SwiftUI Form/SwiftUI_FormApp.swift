1e936f680a4bf9743bcebf265dd0aeb622b7c5da
Xiao Yuan Lv <xiaoyuanlv.31@gmail.com>
2021-12-17 18:58:52 +0630

    Initial Commit

---
+++ SwiftUI Form/SwiftUI_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormApp.swift
+//  SwiftUI Form
+//
+//  Created by Xiao Yuan on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
